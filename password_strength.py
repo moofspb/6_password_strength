f2020f658db15d21f5057601f7974ee473323c36
moofspb <moofspb@mail.ru>
2017-02-23 20:46:55 +0000

    Devided one main function into several small functions

--- password_strength.py
+++ password_strength.py
@@ -2,41 +2,46 @@ import re
 import getpass
 
 
+PASSWORD_MIN_STRENGTH = 1
+
+
+def check_password_length(password):
+    return 2 if len(password) >= 8 else 0
+
+
+def check_password_for_lowercase(password):
+    return 1 if re.search(r'[a-z]+', password) else 0
+
+
+def check_password_for_uppercase(password):
+    return 1 if re.search(r'[A-Z]+', password) else 0
+
+
+def check_password_for_digits(password):
+    return 2 if re.search(r'\d', password) else 0
+
+
+def check_password_for_symbols(password):
+    return 3 if re.search(r'[^A-Za-z0-9]+', password) else 0
+
+
 def get_password_strength(password):
-    password_strength = 1
-    if len(password) >= 8:
-        print('Number of characters: OK!')
-        password_strength += 2
-    else:
-        print('Number of characters: NOT OK!')
-
-    if re.search(r'[a-z]+', password):
-        print('Lowercase letters: OK!')
-        password_strength += 1
-    else:
-        print('Lowercase letters: NOT OK!')
-
-    if re.search(r'[A-Z]+', password):
-        print('Uppercase letters: OK!')
-        password_strength += 1
-    else:
-        print('Uppercase letters: OK!')
-
-    if re.search(r'\d', password):
-        print('Digits: OK!')
-        password_strength += 2
-    else:
-        print('Digits: NOT OK!')
-
-    if re.search(r'[^A-Za-z0-9]+', password):
-        print('Symbols: OK!')
-        password_strength += 3
-    else:
-        print('Symbols: NOT OK!')
-
-    print('Password strength = ' + str(password_strength))
+    password_strength = (check_password_length(password) +
+                         check_password_for_lowercase(password) +
+                         check_password_for_uppercase(password) +
+                         check_password_for_digits(password) +
+                         check_password_for_symbols(password) +
+                         PASSWORD_MIN_STRENGTH)
+    return password_strength
 
 
 if __name__ == '__main__':
+    print('Hi! Let\'s check is your password strong enough?')
+    print('The score is just a number between 1 (weak) and 10 (strong)')
+    print("""Strong password is 8 or more characters in length and consists of:
+          1. Upper-case and lower-case letters
+          2. One ore more numerical digits
+          3. Special symbols, such as #,%,@,$""")
     password = getpass.getpass(prompt='Enter your password: ')
-    get_password_strength(password)
+    password_strength = get_password_strength(password)
+    print('Password strength = ' + str(password_strength))
